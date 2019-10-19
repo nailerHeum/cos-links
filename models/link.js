5d59a02341948882de90f9d1346ecac7914e3ee3
nailerHeum <kingman330@gmail.com>
2019-10-19 18:03:55 +0900

    FEAT doing multiple tries for connecting DB

--- models/link.js
+++ models/link.js
@@ -3,25 +3,36 @@ const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : 'localhost'
 const DB = process.env.DB ? process.env.DB : config.get('DB');
-(async () => {
-  try{
-    if (process.env.DB_HOST === undefined) {
 
-    }
-    await mongoose.connect(`mongodb://${DB_HOST}:27017/${DB}`,
-      { useNewUrlParser: true }
-    );
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb://${DB_HOST}:27017/${DB}`, {
+      useNewUrlParser: true
+    });
   } catch (err) {
     console.log(err);
   }
+}
+
+(async () => {
+  for (let i = 0; i < 5; i++) {
+    try {
+      await connectDB();
+      break;
+    } catch (e) {
+      console.log(`${e} TRYING TO CONNECT DB ${i} TIMES`);
+    }
+  }
+  console.log(`Successfully Connected DB`);
 })();
+
 const linkSchema = new mongoose.Schema({
-    author: String,
-    title: String,
-    description: String, 
-    category: String,
-    url: String,
-    metadata: String,
+  author: String,
+  title: String,
+  description: String,
+  category: String,
+  url: String,
+  metadata: String,
 });
 linkSchema.plugin(mongoosePaginate);
 const Link = mongoose.model('Link', linkSchema);
