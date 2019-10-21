f0bfde564dfca573797825fd2d48651625cab416
nailerHeum <kingman330@gmail.com>
2019-10-21 20:33:45 +0900

    REFACTOR seperated db connection code

---
+++ models/db.js
@@ -0,0 +1,32 @@
+const config = require('config');
+const mongoose = require('mongoose');
+const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : 'localhost'
+const DB = process.env.DB ? process.env.DB : config.get('DB');
+
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(`mongodb://${DB_HOST}:27017/${DB}`, {
+      useNewUrlParser: true
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  const db = mongoose.connection;
+
+  db.on('error', async err => {
+    console.log(err);
+    await sleep(5000);
+    console.log('Trying again...');
+    await mongoose.connect(`mongodb://${DB_HOST}:27017/${DB}`, {
+      useNewUrlParser: true
+    });
+  });
+
+  db.once('open', async () => {
+    console.log('DB Connected');
+    require('./link');
+  });
+}
--- models/link.js
+++ models/link.js
@@ -1,35 +1,5 @@
-const config = require('config');
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
-const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : 'localhost'
-const DB = process.env.DB ? process.env.DB : config.get('DB');
-
-const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
-
-(async () => {
-  try {
-    await mongoose.connect(`mongodb://${DB_HOST}:27017/${DB}`, {
-      useNewUrlParser: true
-    });
-  } catch (err) {
-    console.log(err);
-  }
-
-  const db = mongoose.connection;
-
-  db.on('error', async err => {
-    console.log(err);
-    await sleep(5000);
-    console.log('Trying again...');
-    await mongoose.connect(`mongodb://${DB_HOST}:27017/${DB}`, {
-      useNewUrlParser: true
-    });
-  });
-
-  db.once('open', async () => {
-    console.log('DB Connected');
-  });
-})();
 
 const linkSchema = new mongoose.Schema({
   author: String,
@@ -39,6 +9,7 @@ const linkSchema = new mongoose.Schema({
   url: String,
   metadata: String,
 });
+
 linkSchema.plugin(mongoosePaginate);
 const Link = mongoose.model('Link', linkSchema);
 
