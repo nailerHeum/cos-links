e5ceeb22a1e1a22d95b132b77cd543ebdd515f24
nailerHeum <kingman330@gmail.com>
2019-11-18 22:55:23 +0900

    FIX client side code about admin page

--- client/public/admin/index.js
+++ client/public/admin/index.js
@@ -1,7 +1,9 @@
-import { hot } from 'react-hot-loader/root';
+import {
+  hot
+} from 'react-hot-loader/root';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import App from '../components/App.jsx';
 
 const Hot = hot(App);
-ReactDOM.render(<Hot />, document.querySelector('#root'));
+ReactDOM.render( < Hot / > , document.querySelector('#root'));
--- client/public/constants/configs.js
+++ client/public/constants/configs.js
@@ -1,11 +1,13 @@
-import ACCESS_TOKEN from '../../access-token';
+import {
+  accesskey
+} from '../../../server/config/production.json';
 
 const CONFIGS = {
   // url: './public/resource/localdata.json'
   URL: 'http://13.125.91.246/v1',
 
   HEADER: new Headers({
-    'x-access-token': ACCESS_TOKEN,
+    'x-access-token': accesskey,
     'Content-Type': 'application/json'
   })
 };
