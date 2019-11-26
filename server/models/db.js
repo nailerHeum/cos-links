1d09b66d8f14ea4cb948249dedb919cca71bf0a0
nailerHeum <kingman330@gmail.com>
2019-11-26 17:00:28 +0900

    FIX from node-config module to dotenv module

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,7 @@
-const config = require('config');
-const mongoose = require('mongoose');
-const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : 'localhost'
-const DB = process.env.DB ? process.env.DB : config.get('DB');
+const dotenv = require("dotenv").config({ path: "../config/.env" });
+const mongoose = require("mongoose");
+const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : "localhost";
+const DB = process.env.DB;
 
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
@@ -16,17 +16,17 @@ module.exports = async () => {
 
   const db = mongoose.connection;
 
-  db.on('error', async err => {
+  db.on("error", async err => {
     console.log(err);
     await sleep(5000);
-    console.log('Trying again...');
+    console.log("Trying again...");
     await mongoose.connect(`mongodb://${DB_HOST}:27017/${DB}`, {
       useNewUrlParser: true
     });
   });
 
-  db.once('open', async () => {
-    console.log('DB Connected');
-    require('./link');
+  db.once("open", async () => {
+    console.log("DB Connected");
+    require("./link");
   });
-}
+};
--- server/tests/integration.spec.js
+++ server/tests/integration.spec.js
@@ -1,18 +1,19 @@
-const should = require('chai').should();
-const fs = require('fs');
-const path = require('path');
-const config = require('config');
-const request = require('supertest');
-const server = require('../app');
+const should = require("chai").should();
+const fs = require("fs");
+const path = require("path");
+const config = require("config");
+const request = require("supertest");
+const server = require("../app");
 
-describe('Server', () => {
-  it('서버가 해당 포트에서 동작하는지 테스트', () => {
-    server.port.should.equal(config.get('port'));
+describe("Server", () => {
+  it("서버가 해당 포트에서 동작하는지 테스트", () => {
+    server.port.should.equal(parseInt(process.env.port, 10));
   });
-  it('/ 에서 정상적으로 response를 주는가', (done) => {
-    const resFile = path.resolve(__dirname, '../public/index.html');
-    const resText = fs.readFileSync(resFile).toString()
-    request(server).get('/')
+  it("/ 에서 정상적으로 response를 주는가", done => {
+    const resFile = path.resolve(__dirname, "../public/index.html");
+    const resText = fs.readFileSync(resFile).toString();
+    request(server)
+      .get("/")
       .expect(200)
       .end((err, res) => {
         if (err) {
@@ -24,32 +25,34 @@ describe('Server', () => {
         return;
       });
   });
-  it('get /v1 request', (done) => {
-    request(server).get('/v1')
-      .set('x-access-token', config.get('accesskey'))
+  it("get /v1 request", done => {
+    request(server)
+      .get("/v1")
+      .set("x-access-token", process.env.accesskey)
       .expect(200)
       .end((err, res) => {
         if (err) {
           done(err);
           return;
         }
-        res.body.should.have.property('links');
-        res.body.should.have.property('categories');
+        res.body.should.have.property("links");
+        res.body.should.have.property("categories");
         done();
         return;
       });
   });
-  it('post /v1/link request', (done) => {
-    request(server).post('/v1/link')
-      .set('x-access-token', config.get('accesskey'))
+  it("post /v1/link request", done => {
+    request(server)
+      .post("/v1/link")
+      .set("x-access-token", process.env.accesskey)
       .send({
-        author: 'Corn',
-        title: '스토리보드',
-        description: '아오에스분들 보세여',
-        category: 'iOS',
-        url: 'https://baked-corn.tistory.com/54',
+        author: "Corn",
+        title: "스토리보드",
+        description: "아오에스분들 보세여",
+        category: "iOS",
+        url: "https://baked-corn.tistory.com/54"
       })
-      .set('Accept', 'application/json')
+      .set("Accept", "application/json")
       .expect(201)
       .end((err, res) => {
         if (err) {
